05f419799f1ed31873674667df3ec76a06dc6d8d
pinkSakoora <santrozeson@gmail.com>
2025-07-03 18:37:46 +0530

    Added more obstacle series

--- Assets/ObstacleMover.cs
+++ Assets/ObstacleMover.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class ObstacleMover : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    [SerializeField] float _obsLength;
-    [SerializeField] float _spawnInterval;
+    [SerializeField] float _speed;
+    [SerializeField] float _length;
     private float _timer;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.linearVelocityX = -(_obsLength/_spawnInterval);
+        _rb.linearVelocityX = -_speed;
     }
 
     void Update()
     {
         _timer += Time.deltaTime;
-        if (_timer > 4*_spawnInterval)
+        if (_timer > _length/_speed*4)
         {
             Destroy(gameObject);
         }
